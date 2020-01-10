e03361b14d01720f1a9b51c18061c51a0349b1b6
roman-cf <roman.springer@outlook.com>
2020-01-10 11:48:45 +0100

    first list displayed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,8 +3,13 @@ package sample;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,14 +18,14 @@ import java.util.List;
 import static javafx.scene.paint.Color.*;
 
 public class Main extends Application {
-    private DataAccessTeacher dbaccess;
+    private DataAccessTeacher dbaccessTeacher;
     private ObservableList<Teacher> teachers;
     private ListView<Teacher> listView;
 
     @Override
     public  void init() {
         try  {
-            dbaccess = new  DataAccessTeacher();
+            dbaccessTeacher = new  DataAccessTeacher();
         }
         catch (Exception e) {
             displayException(e);
@@ -29,7 +34,7 @@ public class Main extends Application {
     @Override
     public  void stop() {
         try {
-            dbaccess.closeDb();
+            dbaccessTeacher.closeDb();
         }
         catch (Exception e) {
             displayException(e);
@@ -40,13 +45,43 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("School Statistics");
 
+        // objekte und elemente erstellen
+        Label lblHeadTeacher = new Label("Teachers:");
+            lblHeadTeacher.setStyle("-fx-font-weight:bold");
+        Label lblHeadDetails = new Label("Details to this Teacher:");
+            lblHeadDetails.setStyle("-fx-font-weight:bold");
+        Label lblTeacherID = new Label("ID:");
+            lblTeacherID.setMinWidth(55);
+            lblTeacherID.setAlignment(Pos.BASELINE_RIGHT);
+        TextField txtID = new TextField();
+        Label lblTeacherName = new Label("Name:");
+            lblTeacherName.setMinWidth(55);
+        TextField txtName = new TextField();
+        Label lblTeacherSurname = new Label("Surname:");
+            lblTeacherSurname.setMinWidth(55);
+        TextField txtSurname = new TextField();
+        Label lblTeacherEmail = new Label("Email:");
+            lblTeacherEmail.setMinWidth(55);
+        TextField txtEmail = new TextField();
+
         listView = new ListView<>();
         //listView.getSelectionModel().selectedIndexProperty().addListener(new  ListSelectChangeListener());
         teachers = getDbData();
         listView.setItems(teachers);
 
-        VBox vBoxMain = new VBox(listView);
-        Scene sceneTeacher = new Scene(vBoxMain);
+// Scene Zusammenbauen ---------------------------------------------------------------------------
+
+        VBox vBoxLeft = new VBox(lblHeadTeacher,listView);
+        vBoxLeft.setPadding(new Insets(10,10,10,10));
+        HBox hBoxLblId = new HBox(lblTeacherID,txtID);
+        HBox hBoxLblName = new HBox(lblTeacherName,txtName);
+        HBox hBoxLblSurname = new HBox(lblTeacherSurname,txtSurname);
+        HBox hBoxLblEmail = new HBox(lblTeacherEmail,txtEmail);
+        VBox vBoxmiddle = new VBox(lblHeadDetails,hBoxLblId,hBoxLblName,hBoxLblSurname,hBoxLblEmail);
+        vBoxmiddle.setPadding(new Insets(10,10,10,10));
+
+        HBox mainBox = new HBox(vBoxLeft,vBoxmiddle);
+        Scene sceneTeacher = new Scene(mainBox);
         sceneTeacher.setFill(GRAY);
         primaryStage.setScene(sceneTeacher);
         primaryStage.show();
@@ -55,18 +90,15 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     private  ObservableList<Teacher> getDbData() {
-
         List<Teacher> list = null ;
-
         try  {
-            list = dbaccess.getAllRows();
+            list = dbaccessTeacher.getAllRows();
         }
         catch  (Exception e) {
-
             displayException(e);
         }
-
         ObservableList<Teacher> dbData = FXCollections.observableList(list);
         return  dbData;
     }
